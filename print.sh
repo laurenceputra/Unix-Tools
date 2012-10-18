658d6a0423ec112b85b755c5f90ce8ec004c3b5d
laurenceputra <laurenceputra@gmail.com>
2012-10-18 14:53:25 +0800

    final version of print script

--- print.sh
+++ print.sh
@@ -13,16 +13,17 @@ tmp2=`lpq -Ppstsc-sx | wc -l`
 tmp3=`lpq -Ppstsc-dx | wc -l`
 p3=$(($tmp1+$tmp2+$tmp3))
 
-
 if [ $p1 -le $p2 -a $p1 -le $p3 ]
 then
         echo "psts-dx"
-        #lpr -Ppsts-dx to_print/*.ps
+        lpr -Ppsts-dx to_print/*.ps
 elif [ $p2 -le $p3 ]
 then
         echo "pstsb-dx"
-        #lpr -Ppstsb-dx to_print/*.ps
+        lpr -Ppstsb-dx to_print/*.ps
 else
         echo "pstsc-dx"
-        #lpr -Ppstsc-dx to_print/*.ps
+        lpr -Ppstsc-dx to_print/*.ps
 fi
+
+rm to_print/*.ps
